55faa312540d72d4d8a2e7e4ed1c33f9c7203c8d
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-03 20:32:00 +0300

    feat: add create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,23 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const content = 'I am fresh and young';
+const error = 'FS operation failed';
+const success = 'File was created!';
+
 export const create = async () => {
-    // Write your code here 
+  const folderPath = path.join(__dirname, 'files', 'fresh.txt');
+
+  fs.writeFile(folderPath, content, { flag: 'wx' }, (err) => {
+    if (err) {
+      throw new Error(error);
+    }
+    console.log(success);
+  });
 };
+create();
