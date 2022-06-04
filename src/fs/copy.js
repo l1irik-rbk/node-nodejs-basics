657dc08b351aeaaaa472562bf9870dccc71788d8
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-04 12:27:43 +0300

    feat: add copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,29 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readdir, mkdir, copyFile } from 'node:fs/promises';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const error = 'FS operation failed';
+
 export const copy = async () => {
-  // Write your code here
+  try {
+    const pathFiles = path.join(__dirname, 'files');
+    const pathFilesCopy = path.join(__dirname, 'files-copy');
+    const files = await readdir(pathFiles, { withFileTypes: true });
+    await mkdir(pathFilesCopy);
+
+    for (const file of files) {
+      const oldFilePath = path.join(pathFiles, file.name);
+      const newFilePath = path.join(pathFilesCopy, file.name);
+      await copyFile(oldFilePath, newFilePath);
+    }
+  } catch (err) {
+    if (err) {
+      throw new Error(error);
+    }
+  }
 };
+copy();
