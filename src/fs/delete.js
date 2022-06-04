81e045258a757a12d523ca471dc35fce9056a00c
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-04 13:02:03 +0300

    feat: add delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { rm } from 'node:fs/promises';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const error = 'FS operation failed';
+
 export const remove = async () => {
-    // Write your code here 
+  try {
+    const removePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+    await rm(removePath);
+  } catch (err) {
+    throw new Error(error);
+  }
 };
+remove();
