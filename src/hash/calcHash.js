6751960c53e24828c2938a924db9b508e3f6223f
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-04 18:40:27 +0300

    feat: add hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,23 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as path from 'path';
+import * as crypto from 'crypto';
+import { readFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const hash = crypto.createHash('sha256');
+    const file = await readFile(filePath, { encoding: 'utf-8' });
+    hash.update(file);
+
+    const hex = hash.digest('hex');
+    console.log(hex);
+  } catch (error) {
+    console.error(error);
+  }
 };
+calculateHash();
