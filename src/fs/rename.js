8c716aaac01d688360db9f56fd8994cd13c6bbed
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-04 12:56:23 +0300

    feat: add rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,21 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { rename as renameFile } from 'node:fs/promises';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const error = 'FS operation failed';
+
 export const rename = async () => {
-    // Write your code here 
+  try {
+    const oldPathName = path.join(__dirname, 'files', 'wrongFilename.txt');
+    const newPathName = path.join(__dirname, 'files', 'properFilename.md');
+
+    await renameFile(oldPathName, newPathName);
+  } catch (err) {
+    throw new Error(error);
+  }
 };
+rename();
