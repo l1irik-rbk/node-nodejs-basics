91bff8381253cea744e039610763a2f6930d329a
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-04 20:27:05 +0300

    feat: add compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,25 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { pipeline } from 'stream';
+import * as path from 'path';
+import * as zlib from 'zlib';
+import * as fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const compress = async () => {
-    // Write your code here 
+  const inputPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const outputPath = path.join(__dirname, 'files', 'archive.gz');
+
+  const gzip = zlib.createGzip();
+  const source = fs.createReadStream(inputPath);
+  const destination = fs.createWriteStream(outputPath);
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 };
+compress();
