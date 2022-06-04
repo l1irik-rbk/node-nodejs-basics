389be96abe343634f2f1178620523a853261286b
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-04 19:55:49 +0300

    feat: add transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,17 @@
+import { Transform, pipeline } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+  const readable = process.stdin;
+  const writable = process.stdout;
+
+  const transformation = new Transform({
+    transform(chunk, enc, cb) {
+      const chunkedString = chunk.toString().trim();
+      const reversedChunkedString = chunkedString.split('').reverse().join('');
+      this.push(`${reversedChunkedString}\n`);
+      cb();
+    },
+  });
+  pipeline(readable, transformation, writable, (err) => console.error(err));
 };
+transform();
