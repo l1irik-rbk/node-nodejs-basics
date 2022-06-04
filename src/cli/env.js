d59ee8e370b7500bc43f34336d11e98bccd5b94f
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-04 15:47:31 +0300

    feat: add env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,12 @@
 export const parseEnv = () => {
-    // Write your code here 
+  const env = process.env;
+  const finalArray = [];
+  for (const key in env) {
+    if (key.includes('RSS_')) {
+      finalArray.push(`${key}=${env[key]}`);
+    }
+  }
+  const string = finalArray.join('; ');
+  console.log(string);
 };
+parseEnv();
