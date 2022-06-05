e875af85b7ff9f6c3867371d0e89664b92477bb0
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-05 16:50:21 +0300

    feat: add cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,31 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { spawn } from 'child_process';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filePath = path.join(__dirname, 'files', 'script.js');
+  const newArgs = [filePath, ...args];
+
+  const child = spawn('node', newArgs);
+
+  child.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  process.stdin.on('data', (data) => {
+    child.stdin.write(data.toString().trim());
+  });
+
+  child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 };
+spawnChildProcess(process.argv.slice(2));
