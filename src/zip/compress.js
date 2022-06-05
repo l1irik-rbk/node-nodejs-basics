1af019034888df5ec8ae11eae7bc8abd50acd9a4
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-05 11:29:59 +0300

    fix: compress and decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -2,26 +2,28 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { pipeline } from 'stream';
 import { rm } from 'node:fs/promises';
+import { promisify } from 'node:util';
 import * as path from 'path';
 import * as zlib from 'zlib';
 import * as fs from 'fs';
 
+const pipe = promisify(pipeline);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const compress = async () => {
-  const inputPath = path.join(__dirname, 'files', 'fileToCompress.txt');
-  const outputPath = path.join(__dirname, 'files', 'archive.gz');
+  try {
+    const inputPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const outputPath = path.join(__dirname, 'files', 'archive.gz');
 
-  const gzip = zlib.createGzip();
-  const source = fs.createReadStream(inputPath);
-  const destination = fs.createWriteStream(outputPath);
+    const gzip = zlib.createGzip();
+    const source = fs.createReadStream(inputPath);
+    const destination = fs.createWriteStream(outputPath);
 
-  pipeline(source, gzip, destination, (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-  await rm(inputPath);
+    await pipe(source, gzip, destination);
+    await rm(inputPath);
+  } catch (error) {
+    console.error(error);
+  }
 };
 compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -2,26 +2,28 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { pipeline } from 'stream';
 import { rm } from 'node:fs/promises';
+import { promisify } from 'node:util';
 import * as path from 'path';
 import * as zlib from 'zlib';
 import * as fs from 'fs';
 
+const pipe = promisify(pipeline);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const decompress = async () => {
-  const inputPath = path.join(__dirname, 'files', 'archive.gz');
-  const outputPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  try {
+    const inputPath = path.join(__dirname, 'files', 'archive.gz');
+    const outputPath = path.join(__dirname, 'files', 'fileToCompress.txt');
 
-  const unzip = zlib.createUnzip();
-  const source = fs.createReadStream(inputPath);
-  const destination = fs.createWriteStream(outputPath);
+    const unzip = zlib.createUnzip();
+    const source = fs.createReadStream(inputPath);
+    const destination = fs.createWriteStream(outputPath);
 
-  pipeline(source, unzip, destination, (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-  await rm(inputPath);
+    await pipe(source, unzip, destination);
+    await rm(inputPath);
+  } catch (error) {
+    console.error(error);
+  }
 };
 decompress();
